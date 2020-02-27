1d8ce60d9bcbbe8028067ba5e614ff554d7e2b8d
tpetre1 <61437173+tpetre1@users.noreply.github.com>
2020-02-27 21:10:37 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Logo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input N = ");
+            int N = int.Parse(Console.ReadLine());
+            int M = 10 * N;
+            char[,] Logo = new Char[N + 1, M];
+
+            for (int i = 0; i <= N; i++)            // Init
+                for (int j = 0; j < M; j++)
+                    Logo[i, j] = '-';
+
+            // First Letter M
+
+            for (int i = 0; i <= N; i++)            // First Up Movement
+                for (int j = N - i; j < 2 * N - i; j++)
+                    Logo[i, j] = '*';
+
+            for (int i = N; i >= 0; i--)            // First Down Movement
+                for (int j = N + i; j < 2 * N + i; j++)
+                    Logo[i, j] = '*';
+
+            for (int i = 0; i <= N; i++)            // Second Up Movement
+                for (int j = 3 * N - i; j < 4 * N - i; j++)
+                    Logo[i, j] = '*';
+
+            for (int i = N; i >= 0; i--)            // Second Down Movement
+                for (int j = 3 * N + i; j < 4 * N + i; j++)
+                    Logo[i, j] = '*';
+
+            // Second Letter M
+
+            for (int i = 0; i <= N; i++)            // First Up Movement
+                for (int j = 6 * N - i; j < 7 * N - i; j++)
+                    Logo[i, j] = '*';
+
+            for (int i = N; i >= 0; i--)            // First Down Movement
+                for (int j = 6 * N + i; j < 7 * N + i; j++)
+                    Logo[i, j] = '*';
+
+            for (int i = 0; i <= N; i++)            // Second Up Movement
+                for (int j = 8 * N - i; j < 9 * N - i; j++)
+                    Logo[i, j] = '*';
+
+            for (int i = N; i >= 0; i--)            // Second Down Movement
+                for (int j = 8 * N + i; j < 9 * N + i; j++)
+                    Logo[i, j] = '*';
+
+            for (int i = 0; i <= N; i++)            // Result
+            {
+                for (int j = 0; j < M; j++)
+                    Console.Write(Logo[i, j]);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("The end!");
+        }
+    }
+}
